db931712d8034d84ec0fb33ef37f5b78bdaff65e
SpiNoice01 <fadhilaagil72@gmail.com>
2024-09-25 00:09:11 +0700

    Tweaking dan designing tampilan biodata

--- 01_Pengenalan_Flutter/flutter_application_1/lib/main.dart
+++ 01_Pengenalan_Flutter/flutter_application_1/lib/main.dart
@@ -1,71 +1,197 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 1), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple,
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          leading: const Icon(Icons.menu),
-          title: const Center(
-            child: Text(
-              "BIODATA",
-              style: TextStyle(color: Colors.black),
+        appBar: CustomAppBar(
+          title: "BIODATA",
+          backgroundColor: Colors.purple[300]!,
+          actions: const [Icon(Icons.menu)],
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.amber,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "img/Agil.jpg",
-              width: 200.0,
-              height: 200.0,
-            ),
-            const Text(
-              "NAMA : Fadhila Agil Permana\nNIM : 2211104006",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: AnimatedOpacity(
+                opacity: _opacity,
+                duration: const Duration(seconds: 2),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "img/Agil.jpg",
+                      width: 150.0,
+                      height: 150.0,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Container(
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10.0,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Table(
+                        columnWidths: const {
+                          0: IntrinsicColumnWidth(),
+                        },
+                        children: const [
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  "Nama ",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  ": Fadhila Agil Permana",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  "NIM ",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  ": 2211104006",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  "Jenis Kelamin ",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  ": Laki-Laki",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  "Tanggal Lahir ",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  ": 8 Juli 2005",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              textAlign: TextAlign.center,
             ),
-            const Text(
-              "Jenis Kelamin : Laki-Laki",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "Tanggal Lahir : 8 Juli 2005",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "Asal : Cirebon",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final Color backgroundColor;
+  final List<Widget> actions;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    required this.backgroundColor,
+    required this.actions,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      backgroundColor: backgroundColor,
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
