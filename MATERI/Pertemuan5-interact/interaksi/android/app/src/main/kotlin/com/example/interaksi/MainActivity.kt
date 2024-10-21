2265920f8ee7d6dc27637bb1579588aab4080096
SpiNoice01 <fadhilaagil72@gmail.com>
2024-10-22 00:47:33 +0700

    copas doc flutter class navigasi bar

---
+++ MATERI/Pertemuan5-interact/interaksi/android/app/src/main/kotlin/com/example/interaksi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.interaksi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ PRAKTIKUM/05_Flexible/TP/pindah2/android/app/src/main/kotlin/com/example/pindah2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pindah2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
