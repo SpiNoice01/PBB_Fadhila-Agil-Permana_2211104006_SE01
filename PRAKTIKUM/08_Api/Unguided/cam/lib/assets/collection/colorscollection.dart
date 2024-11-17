f92c7ef733b4d3944663eb02cb91d8555ee6a923
SpiNoice01 <fadhilaagil72@gmail.com>
2024-11-17 18:16:01 +0700

    idk, push

---
+++ PRAKTIKUM/08_Api/Unguided/cam/lib/assets/collection/colorscollection.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Colorscollection {
+  Color background = const Color(0xFF092635);
+  Color hijaugelap = const Color(0xFF092635);
+  Color hijaumedium = const Color(0xFF5C8374);
+  Color hijaumuda = const Color(0xFF092635);
+}
