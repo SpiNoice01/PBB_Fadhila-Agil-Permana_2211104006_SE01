a2ebe97d1f8b22f8a122861342a7ed3e19e48dc4
SpiNoice01 <fadhilaagil72@gmail.com>
2025-01-03 14:10:02 +0700

    mengambil asset2 dan element

---
+++ PRAKTIKUM/COTS/cots/lib/design_system/colors_collection.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColorCollection {
+  static const Color color1 = Color.fromRGBO(156, 156, 156, 1);
+  static const Color color2 = Color.fromRGBO(32, 32, 32, 1);
+  static const Color color3 = Color.fromRGBO(236, 240, 255, 1);
+  static const Color color4 = Color.fromRGBO(0, 170, 19, 1);
+  static const Color color5 = Color.fromRGBO(0, 0, 0, 1);
+  static const Color color6 = Color.fromRGBO(0, 129, 160, 1);
+  static const Color color7 = Color.fromRGBO(0, 0, 0, 0.6);
+  static const Color color8 = Color.fromRGBO(48, 176, 88, 1);
+  static const Color color9 = Color.fromRGBO(0, 108, 2, 1);
+  static const Color color10 = Color.fromRGBO(142, 142, 142, 1);
+  static const Color color11 = Color.fromRGBO(247, 247, 247, 1);
+  static const Color color12 = Color.fromRGBO(240, 100, 0, 1);
+  static const Color color13 = Color.fromRGBO(223, 25, 149, 1);
+  static const Color color14 = Color.fromRGBO(147, 50, 142, 1);
+  static const Color color15 = Color.fromRGBO(238, 39, 55, 1);
+  static const Color color16 = Color.fromRGBO(142, 142, 142, 0.2);
+  static const Color color17 = Color.fromRGBO(255, 255, 255, 1);
+  static const Color color18 = Color.fromRGBO(194, 194, 194, 1);
+  static const Color color19 = Color.fromRGBO(153, 158, 176, 1);
+  static const Color color20 = Color.fromRGBO(0, 0, 0, 1);
+  static const Color color21 = Color.fromRGBO(0, 0, 0, 1);
+  static const Color color22 = Color.fromRGBO(0, 73, 1, 1);
+  static const Color color23 = Color.fromRGBO(114, 114, 114, 1);
+  static const Color color24 = Color.fromRGBO(246, 248, 255, 1);
+}
